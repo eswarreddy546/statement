dca85104d08e16cc60c2668c472aff68912e72b8
Asri <asri>
2025-09-22 19:24:00 +0530

    first commit

--- first.sh
+++ first.sh
@@ -4,5 +4,18 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
 
-echo "print this error"
+echo "print the first  error message"
+
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+
+echo " print the second error message"
+
+else 
+
+echo " Install mysql command"
+
 fi
