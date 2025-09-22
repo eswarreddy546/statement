992c2dd2a4b38a2a27bd041f1549260a6886f0c5
Asri <asri>
2025-09-22 12:57:56 +0530

    first commit

---
+++ ifelseif.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Number1=$1
+
+if [ $Number1 -gt 100 ]; then
+
+echo "Number is greater then 100 $Number1"
+
+elif [ $Number1 -eq 100 ]; then
+
+ echo " Number is lessthen 100 $Number1"
+
+
+else
+echo " Number is lessthen 100 $Number1"
+
+fi
