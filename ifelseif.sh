84fee46da9c74c8f2585220ed3c650b957a33375
Asri <asri>
2025-09-22 13:03:51 +0530

    first commit

--- ifelseif.sh
+++ ifelseif.sh
@@ -4,11 +4,11 @@ Number1=$1
 
 if [ $Number1 -gt 100 ]; then
 
-echo "Number is greater then 100 $Number1"
+echo "Number is greater than 100 $Number1"
 
 elif [ $Number1 -eq 100 ]; then
 
- echo " Number is lessthen 100 $Number1"
+ echo " Number is equalto 100 $Number1"
 
 
 else
