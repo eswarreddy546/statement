4e3a8f7d87ceb09380a7af77aa1f05d3a12fe5ef
Asri <asri>
2025-09-26 13:45:53 +0530

    push the new lines

---
+++ fun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USE=$(id -u)
+
+if [ $USE -ne 0 ]; then
+ 
+ echo "eror occured"
+exit 1;
+else
+echo "error is not occured"
+
+fi
+
+Valid (){
+
+if [ $1 -ne 0 ]; then
+
+echo "$2 is not installed"
+exit 1;
+else
+echo "$2 is sucessfully instaeed"
+
+fi
+
+}
+dnf install mysql -y;
+ Valid $? "mysql"
+ 
+ dnf install nginx -y;
+ Valid $? "nginx"
+
+ dnf install python3 -y;
+ Valid $? "python"
