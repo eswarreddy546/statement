b6ca611266b83711b25ec54a5af4ec90212a6e81
Asri <asri>
2025-09-26 14:50:05 +0530

    push the new lines

---
+++ existskip.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+User=$(id -u)
+
+if [ $User -ne 0 ]; then
+
+echo "Error is not occured "
+
+fi
+
+valid ()
+{
+    if [ $1 -ne 0 ]; then 
+echo "error is occured $2 is "
+
+else
+echo "Sucessfully exicuted $2 "
+ 
+ fi
+}
+
+
+
+dnf list installed mysql;
+if [ $? -ne 0 ]; then
+dnf install mysql -y;
+valid $? "mysql"
+else 
+echo : "Alredy mysql is avalible please skip it....."
+
+fi
+
+
+dnf list installed nginx;
+if [ $? -ne 0 ]; then
+dnf install nginx -y;
+valid $? "mysql"
+else 
+echo : "Alredy nginx is avalible please skip it....."
+
+fi
+
+dnf list installed python3;
+if [ $? -ne 0 ]; then
+dnf install python3 -y;
+valid $? "m"
+else 
+echo : "Alredy python3  is avalible please skip it....."
+
+fi
