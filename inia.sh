d17cfe1bff447bfb2b7a2baa5207baddf859cbd2
Asri <asri>
2025-09-26 10:59:27 +0530

    push latest one

--- inia.sh
+++ inia.sh
@@ -18,6 +18,16 @@ echo "not running mysql command"
 
 exit 1;
 else
-
 echo " command mysql is sucessfullly running "
+fi
+
+dnf install nginx -y;
+
+if [ $? -ne 0 ]; then
+
+echo "not running nginx command"
+
+exit 1;
+else
+echo " command nginx is sucessfullly running "
 fi
