d80c43608500cea517242e8eb18d5fe745f04f19
Asri <asri>
2025-09-27 13:04:56 +0530

    latest command

--- practice1.sh
+++ practice1.sh
@@ -2,13 +2,21 @@
 UAID=$(id -u)
 
 if [ $UAID -ne 0 ]; then
-
 echo "error: not privilage acess "
 exit 1;
 
 else
-
  echo "Hello world"
-
 fi
 
+dnf install mysql -y;
+
+if [ $? -ne 0 ]; then
+
+echo " not excutd "
+
+else
+
+echo " sucessfully excuted "
+
+fi
